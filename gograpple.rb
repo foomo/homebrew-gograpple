c4405208bc6769bf6d79de4efe82aea03cd76fc2
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-03 16:42:56 +0200

    Brew formula update for gograpple version v0.0.9

--- gograpple.rb
+++ gograpple.rb
@@ -2,16 +2,16 @@
 class Gograpple < Formula
   desc "CLI utility manage infrastructure as code with helm"
   homepage "https://github.com/foomo/gograpple"
-  version "0.0.8"
+  version "0.0.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/foomo/gograpple/releases/download/v0.0.8/gograpple_0.0.8_darwin_amd64.tar.gz"
-    sha256 "8dac855c902862903f3546ef4c2e8f020560057a86c27d0ad0e2cdc3cb3e9715"
+    url "https://github.com/foomo/gograpple/releases/download/v0.0.9/gograpple_0.0.9_darwin_amd64.tar.gz"
+    sha256 "915b23f898b4a4e5b0b6ef184a426414337ff65e97365dfa7a5f3a2015f9ef7a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gograpple/releases/download/v0.0.8/gograpple_0.0.8_linux_amd64.tar.gz"
-      sha256 "b2b5a6e5ea9276ec4dfce730a154d43e22cd3bca1ca9d30c25e85cc3b7a0948b"
+      url "https://github.com/foomo/gograpple/releases/download/v0.0.9/gograpple_0.0.9_linux_amd64.tar.gz"
+      sha256 "3fafcb2be004a90ac9ec7e7e19995a4662e933f3622d83b6054d9c1fdb5d5f54"
     end
   end
 
